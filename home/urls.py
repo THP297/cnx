861034b972ada10b8b7ad5a2bb9cacca36b7eacc
Ngo Anh <nvanh31798@gmail.com>
2025-02-16 21:05:50 +0700

    update component use for new template

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import (
+    address_info_form,
+    contact_info_form,
+    customer_info_form,
     index,
     manage_telesales_campaign,
+    payment_info_form,
+    phone_info_form,
+    product_info_form,
 )
 
 app_name = 'home'
@@ -9,4 +15,11 @@ app_name = 'home'
 urlpatterns= [
     path('index/', index, name='index'),
     path('manage-telesales/campaign/', manage_telesales_campaign, name='manage-telesales-campaign'),
+    path('manage-telesales/campaign/customer_info/', customer_info_form, name='customer-info-form'),
+    path('manage-telesales/campaign/contact_info/', contact_info_form, name='contact-info-form'),
+    path('manage-telesales/campaign/address_info/', address_info_form, name='address-info-form'),
+    path('manage-telesales/campaign/payment_info/', payment_info_form, name='payment-info-form'),
+    path('manage-telesales/campaign/product_info/', product_info_form, name='product-info-form'),
+    path('manage-telesales/campaign/phone_info/', phone_info_form, name='phone-info-form'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -6,6 +6,41 @@ from django.http import HttpRequest, HttpResponse
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "home/index.html")
 
+def customer_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "customer-info",
+    }
+    return render(request, "home/components/customer-info-form.html", context)
+
+def contact_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "contact-info",
+    }
+    return render(request, "home/components/contact-info-form.html", context)
+
+def address_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "address-info",
+    }
+    return render(request, "home/components/address-info-form.html", context)
+
+def payment_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "payment-info",
+    }
+    return render(request, "home/components/payment-info-form.html", context)
+
+def product_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "product-info",
+    }
+    return render(request, "home/components/product-info-form.html", context)
+
+def phone_info_form(request: HttpRequest) -> HttpResponse:
+    context = {
+        "active_tab": "phone-info",
+    }
+    return render(request, "home/components/phone-info-form.html", context)
 
 def manage_telesales_campaign(request: HttpRequest) -> HttpResponse:
     context = {
