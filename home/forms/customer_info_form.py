d34e87b5c84823d5b42f38a8e499b18d4c3f5599
Ngo Anh <nvanh31798@gmail.com>
2025-02-19 01:10:41 +0700

    update the text field and select field

--- home/forms/customer_info_form.py
+++ home/forms/customer_info_form.py
@@ -25,17 +25,13 @@ from home.models.customer_info import CustomerInfo
 class CustomerInfoForm(forms.Form):
     name = forms.CharField(required=False, label="Tên KH")
     id_number = forms.CharField(max_length=20, label="CCCD")
-    day_of_birth = forms.DateField(label="Ngày sinh", required=False)
+    day_of_birth = forms.DateField(label="Ngày sinh", required=True)
     gender = forms.CharField(label="Giới tính", required=False)
     contract_number = forms.CharField(label="Số hợp đồng", required=False)
     company_name = forms.CharField(label="Tên công ty", required=False)
-    company_address = forms.CharField(label="Địa chỉ công ty", required=False)
-    company_district = forms.CharField(
-        label="Địa chỉ công ty - Quận/Huyện", required=False
-    )
-    company_province = forms.CharField(
-        label="Địa chỉ công ty - Tỉnh/TP", required=False
-    )
+    company_address = forms.CharField(label="Địa chỉ công ty", required=True)
+    company_district = forms.CharField(label="Địa chỉ công ty - Quận/Huyện", required=False)
+    company_province = forms.CharField(label="Địa chỉ công ty - Tỉnh/TP", required=False)
 
     def save(self, commit=True):
         data = self.cleaned_data
